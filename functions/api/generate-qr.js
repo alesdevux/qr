0e011171e9753db3b215dd050d1a6327eccb7895
Ales <aleswebgit@gmail.com>
2022-09-21 13:16:44 +0200

    functional generate qr

--- functions/api/generate-qr.js
+++ functions/api/generate-qr.js
@@ -1,15 +1,21 @@
-export async function onRequestGet(context) {
-  // Contents of context object
-  const {
-    request, // same as existing Worker API
-    env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
-  } = context;
+import qr from 'qrcode';
 
+export async function onRequestPost(context) {
+  const { request } = context;
+  const { url } = await request.json();
 
-  return new Response("Hello, world!");
+  const qrImage = await qr.toString(url, { 
+    type: 'svg',
+    color: {
+      dark: '#FFF',
+      light: '#0000'
+    }
+  });
+
+  return new Response(JSON.stringify({svg: qrImage}), {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
 
 }
